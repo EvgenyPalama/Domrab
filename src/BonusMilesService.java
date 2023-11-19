6706d293e4c8155b5076ddbb6f9be9f3cf4dbec4
Evgeniy <palama15@mail.ru>
2023-11-19 21:18:50 +0500

    Initial

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,17 +1,17 @@
 public class BonusMilesService {
-    public int calculate(int price){
+    public int calculate(int price) {
 
         int bonusScale = 20;
         int uploadVolume = 10000;
         int bonus;
 
-        if (uploadVolume >= 1000){
+        if (uploadVolume >= 1000) {
             bonus = uploadVolume / bonusScale;
         } else {
             bonus = 0;
         }
 
-        int totalAccount =uploadVolume + bonus;
+        int totalAccount = uploadVolume + bonus;
 
 
         System.out.println("Ваш бонус составил " + bonus + " руб.");
