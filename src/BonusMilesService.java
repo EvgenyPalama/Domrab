606dad0b87d479f5a6cd1908d503eef8e11582fa
Evgeniy <palama15@mail.ru>
2023-11-19 23:59:53 +0500

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,22 +2,9 @@ public class BonusMilesService {
     public int calculate(int price) {
 
         int bonusScale = 20;
-        int uploadVolume = 10000;
-        int bonus;
 
-        if (uploadVolume >= 1000) {
-            bonus = uploadVolume / bonusScale;
-        } else {
-            bonus = 0;
-        }
+        int bonus = price / bonusScale;
 
-        int totalAccount = uploadVolume + bonus;
-
-
-        System.out.println("Ваш бонус составил " + bonus + " руб.");
-        System.out.println("Ваш баланс " + totalAccount + " руб.");
         return bonus;
     }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 10000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
     }
 }
-
-
-
 
 
